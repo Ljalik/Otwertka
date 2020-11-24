e9c0525c6f5d4aed64d8d8cb285f7a2e3760e6a6
opilane <opilane@HP-CZC6297B9X>
2020-11-24 12:29:21 +0200

    Почти доделано

--- Otwertka/Otwertka/MainPage.xaml.cs
+++ Otwertka/Otwertka/MainPage.xaml.cs
@@ -8,11 +8,23 @@ using Xamarin.Forms;
 
 namespace Otwertka
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : MasterDetailPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+            Detail = new NavigationPage(new News());
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Comrads());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new News());
         }
     }
 }
